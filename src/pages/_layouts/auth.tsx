fc05907b4ddcc9c53a4c135bc9339a7443531a63
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-06-17 07:42:38 -0300

    Página de login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,20 @@
+import { Store } from "lucide-react";
 import { Outlet } from "react-router";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+      <div className="flex items-center gap-3 text-lg text-foreground">
+        <Store className="h-5 w-5"/>
+        <span className="font-semibold">market.place</span>
+      </div>
+      <footer className="text-sm">
+        Painel do parceiro &copy; market.place - {new Date().getFullYear()}
+      </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
