7932d56e2b9e43b55b241d667e6f7aec7ea07006
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-09-28 03:39:13 -0300

    layout dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -11,14 +11,14 @@ export function Dasboard() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-3 gap-4">
           <MonthRevenueCard />
           <MonthOrdersAmountCard />
           <DayOrdersAmountCard />
           
         </div>
 
-        <div className="grid grid-cols-9 gap-4">
+        <div className="grid grid-cols-3 gap-4">
           <RevenueChart />
           
         </div>
